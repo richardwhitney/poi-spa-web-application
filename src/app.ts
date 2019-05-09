9d89365f3b1547f9c7407354f7ab92f324b38573
Richard Whitney <rwhitney32@gmail.com>
2019-05-09 16:02:02 +0100

    added basic site navigation

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,27 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
 
 export class App {
 
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'poi'],
+        name: 'POI',
+        moduleId: PLATFORM.moduleName('views/points'),
+        nav: true,
+        title: 'POI'
+      },
+      {
+        route: 'categories',
+        name: 'categories',
+        moduleId: PLATFORM.moduleName('views/categories'),
+        nav: true,
+        title: 'Categories'
+      }
+    ]);
+    this.router = router;
+  }
 }
