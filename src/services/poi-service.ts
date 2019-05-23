2973f0d918bd79b84fd09fe4ec82b2ea92735a39
Richard Whitney <rwhitney32@gmail.com>
2019-05-23 16:06:16 +0100

    basic integration of server using api

--- src/services/poi-service.ts
+++ src/services/poi-service.ts
@@ -2,7 +2,7 @@ import { inject, Aurelia } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { PLATFORM } from 'aurelia-pal';
 import { EventAggregator } from 'aurelia-event-aggregator';
-import { Point, Category, User } from "./poi-types";
+import { Point, Category, User, RawCategory, RawPoint } from "./poi-types";
 import { HttpClient} from "aurelia-http-client";
 
 @inject(HttpClient,EventAggregator, Aurelia, Router)
@@ -11,31 +11,46 @@ export class PoiService {
   points: Point[] = [];
   categories: Category[] = [];
   users: Map<string, User> = new Map();
+  usersById: Map<string, User> = new Map();
 
   constructor(private httpClient: HttpClient, private ea: EventAggregator, private au: Aurelia, private router: Router) {
     httpClient.configure(http => {
-      http.withBaseUrl('http://localhost:8080');
+      http.withBaseUrl('http://localhost:3000');
     });
     this.getUsers();
-    this.getPoints();
-    console.log('PoiService con: ' + this.points);
   }
 
   async getUsers() {
-    const response = await this.httpClient.get('/api/users.json');
+    const response = await this.httpClient.get('/api/users');
     const users = await response.content;
     users.forEach(user => {
       this.users.set(user.email, user);
+      this.usersById.set(user._id, user);
     });
   }
 
-  signup(firstName: string, lastName: string, email: string, password: string) {
+  async signup(firstName: string, lastName: string, email: string, password: string) {
+    const user = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    };
+    const response = await this.httpClient.post('/api/users', user);
+    const newUser = await response.content;
+    this.users.set(newUser.email, newUser);
+    this.usersById.set(newUser._id, newUser);
+    await this.getPoints();
+    await this.getCategories();
     this.changeRouter(PLATFORM.moduleName('app'));
+    return false;
   }
 
   async login(email: string, password: string) {
     const user = this.users.get(email);
     if (user && (user.password === password)) {
+      await this.getPoints();
+      await this.getCategories();
       this.changeRouter(PLATFORM.moduleName('app'));
       return true;
     }
@@ -49,8 +64,19 @@ export class PoiService {
   }
 
   async getPoints() {
-    const response = await this.httpClient.get('/api/points.json');
-    this.points = await response.content;
+    const response = await this.httpClient.get('/api/points');
+    const rawPoints : RawPoint[] = await response.content;
+    rawPoints.forEach(rawPoint => {
+      const point = {
+        name: rawPoint.name,
+        description: rawPoint.description,
+        imageURL: rawPoint.imageUrl,
+        addedBy: this.usersById.get(rawPoint.addedBy),
+        _id: rawPoint._id
+      };
+      this.points.push(point);
+    });
+    console.log('Points:');
     console.log(this.points);
   }
 
@@ -59,18 +85,43 @@ export class PoiService {
       name: name,
       description: description
     };
-    this.points.push(point);
-    category.points.push(point);
+    //this.points.push(point);
+    //category.points.push(point);
     console.log('Point added: ' + point);
     console.log('Category points: ' + category.points);
+  }
+
+  async getCategories() {
+    const response = await this.httpClient.get('/api/categories');
+    const rawCategories: RawCategory[] = await response.content;
+    console.log('***Points***: ' + this.points);
+
+    rawCategories.forEach(rawCategory => {
+      let pointsArray: any[] =[];
+      rawCategory.points.forEach(pointString => {
+        console.log('Point Id: ' + pointString);
+        let p : Point = this.points.find(point => pointString == point._id);
+        console.log('Point Object: ' + p);
+        pointsArray.push(p);
+      });
+      const category = {
+        name: rawCategory.name,
+        points: pointsArray,
+        _id: rawCategory._id
+      }
+      this.categories.push(category);
+    });
+    console.log('Categories:');
+    console.log(this.categories);
   }
 
   async addCategory(name: string,) {
     const category = {
       name: name,
-      points: []
     };
-    this.categories.push(category);
+    const response = await this.httpClient.post('/api/categories', category);
+    const newCategory = await response.content;
+    this.categories.push(newCategory);
     console.log('Added category: ' + category.name);
   }
 
--- src/services/poi-types.ts
+++ src/services/poi-types.ts
@@ -1,16 +1,35 @@
+export interface RawCategory {
+  name: string;
+  points: string[];
+  _id: string;
+}
+
 export interface Category {
   name: string;
   points: Point[];
+  _id: string
 }
 
+export interface RawPoint {
+  name: string;
+  description: string;
+  imageUrl?: string;
+  addedBy: string;
+  _id: string;
+
+}
 export interface Point {
   name: string;
   description: string;
+  imageUrl?: string;
+  addedBy: User;
+  _id: string;
 }
 
 export interface User {
-  firstName: string,
-  lastName: string,
-  email: string,
-  password: string
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  _id: string;
 }
